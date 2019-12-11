4737978c2670a0923b58096f62954f7d73ee91d7
AidansCode <amurphey7@gmail.com>
2019-12-11 01:39:13 -0500

    Spacing

--- models/UserSession.js
+++ models/UserSession.js
@@ -2,18 +2,17 @@ var mongoose = require('mongoose');
 
 var UserSessionSchema = new mongoose.Schema({
   userId: {
-		type: String,
-		default: ''
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now()
-	},
-	isDeleted: {
-		type: Boolean,
-		default: false
-	}
+    type: String,
+    default: ''
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
 });
-
 
 module.exports = mongoose.model('UserSession', UserSessionSchema);
